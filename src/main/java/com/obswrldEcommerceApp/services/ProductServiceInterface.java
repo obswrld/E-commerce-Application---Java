2a3d3be4a19da35dca9d041ea1fc9e680f76941c
Republic Oba <republicoba1@gmail.com>
2025-10-04 23:11:56 +0100

    new works

---
+++ src/main/java/com/obswrldEcommerceApp/services/ProductServiceInterface.java
@@ -0,0 +1,4 @@
+package com.obswrldEcommerceApp.services;
+
+public interface ProductServiceInterface {
+}
--- src/test/java/com/obswrldEcommerceApp/services/UserServiceImplementationTest.java
+++ src/test/java/com/obswrldEcommerceApp/services/UserServiceImplementationTest.java
@@ -3,7 +3,9 @@ package com.obswrldEcommerceApp.services;
 import com.obswrldEcommerceApp.data.models.Role;
 import com.obswrldEcommerceApp.data.models.User;
 import com.obswrldEcommerceApp.data.repositories.UserRepositories;
+import com.obswrldEcommerceApp.dtos.Request.UserLoginRequest;
 import com.obswrldEcommerceApp.dtos.Request.UserRegistrationRequest;
+import com.obswrldEcommerceApp.dtos.Response.UserLoginResponse;
 import com.obswrldEcommerceApp.dtos.Response.UserResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -60,6 +63,38 @@ class UserServiceImplementationTest {
 
         assertThat(userResponse.getUserId()).isEqualTo("1");
         assertThat(userResponse.getEmail()).isEqualTo("johnmike1@gmail.com");
+    }
+
+    @Test
+    public void testLoinUserSuccessful(){
+        User user = User.builder()
+                .email("johnmike1@gmail.com")
+                .password("encoded567tr")
+                .build();
+
+        when(userRepositories.findByEmail("johnmike1@gmail.com"))
+                .thenReturn(Optional.of(user));
+        when(bCryptPasswordEncoder.matches("567tr", "encoded567tr")).thenReturn(true);
+        UserLoginRequest userLoginRequest = new UserLoginRequest("johnmike1@gmail.com","567tr");
+        UserLoginResponse userLoginResponse = userService.login(userLoginRequest);
+
+        assertThat(userLoginResponse.getMessage()).isEqualTo("Login Successful");
+    }
+
+    @Test
+    public void testLoginUserFailed(){
+        User user = User.builder()
+                .email("johnmike1@gmail.com")
+                .password("encoded567tr")
+                .build();
+
+        when(userRepositories.findByEmail("johnmike1@gmail.com"))
+                .thenReturn(Optional.of(user));
+        when(bCryptPasswordEncoder.matches("wrong", "encoded567tr")).thenReturn(false);
+        UserLoginRequest userLoginRequest = new UserLoginRequest("johnmike1@gmail.com","wrong");
+        UserLoginResponse userLoginResponse = userService.login(userLoginRequest);
+
+        assertThat(userLoginResponse.getMessage()).isEqualTo("Invalid Password");
     }
 
 }
